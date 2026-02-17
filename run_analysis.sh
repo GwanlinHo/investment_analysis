258442ddf41096a02991634ab7cbcbb68418281d
Gwanlin Ho <gwanlin@gmail.com>
2026-02-17 21:26:57 +0800

    chore: exclude and auto-cleanup temporary html fragments

--- run_analysis.sh
+++ run_analysis.sh
@@ -8,3 +8,6 @@ cd /home/pi/WorkDir/investment_analysis/
 # 3. 執行 Gemini CLI 進行 AI 分析
 export PATH=$PATH:/home/pi/.config/nvm/versions/node/v22.17.0/bin
 gemini -p 'investment analysis' -y
+
+# 4. 順利產生後，刪除暫時的 HTML 檔案
+rm -f ai_analysis.html tw_macro_table.html us_macro_table.html weekly_news.html
